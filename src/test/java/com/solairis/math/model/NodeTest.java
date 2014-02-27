7cb3b27405c870b3ba987ac456f9c3340154737f
Joshua P Johnson <joshjohnson@trulia.com>
2014-02-26 19:04:05 -0800

    First failing test

---
+++ src/test/java/com/solairis/math/model/NodeTest.java
@@ -0,0 +1,18 @@
+
+package com.solairis.math.model;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author joshjohnson
+ */
+public class NodeTest {
+	
+	@Test
+	public void verify_test_works() {
+		assertTrue(false);
+	}
+	
+}
