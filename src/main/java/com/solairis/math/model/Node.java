1d750bce9de0ac60b7d32489bcbccadab8ea194f
Joshua P Johnson <joshjohnson@trulia.com>
2014-02-26 19:08:23 -0800

    Verify and add methods we'll probably need

--- src/main/java/com/solairis/math/model/Node.java
+++ src/main/java/com/solairis/math/model/Node.java
@@ -7,7 +7,35 @@ package com.solairis.math.model;
  */
 public class Node {
 	
+	private final Integer value;
+	private final String operator;
+	private final Node left;
+	private final Node right;
+
+	public Node(Integer value, String operator, Node left, Node right) {
+		this.value = value;
+		this.operator = operator;
+		this.left = left;
+		this.right = right;
+	}
+	
 	public Node left() {
+		return this.left;
+	}
+	
+	public Node right() {
+		return this.right;
+	}
+	
+	public Integer value() {
+		return this.value;
+	}
+	
+	public String operator() {
+		return this.operator;
+	}
+	
+	public Integer calculate() {
 		return null;
 	}
 	
--- src/test/java/com/solairis/math/model/NodeTest.java
+++ src/test/java/com/solairis/math/model/NodeTest.java
@@ -2,6 +2,7 @@
 package com.solairis.math.model;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,14 +11,34 @@ import org.junit.Test;
  */
 public class NodeTest {
 	
-	@Test
-	public void verify_test_works() {
-		assertTrue(true);
+	private Node node;
+	
+	@Before
+	public void setUp() {
+		this.node = new Node(null, "+", null, null);
 	}
 	
 	@Test
-	public void verify_Node_class_exists() {
-		Node node = new Node();
+	public void verify_Node_has_left() {
+		Node left = this.node.left();
+	}
+	
+	@Test
+	public void verify_Node_has_right() {
+		Node right = this.node.right();
+	}
+	
+	@Test
+	public void verify_Node_has_value() {
+		Integer value = this.node.value();
+	}
+	
+	@Test public void verify_Node_has_operator() {
+		String operator = this.node.operator();
+	}
+	
+	@Test public void verify_Node_has_calculate() {
+		this.node.calculate();
 	}
 	
 }
